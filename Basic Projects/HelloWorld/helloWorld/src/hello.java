6ab6d9d1d355f7bc1630b056f3856fba5e0834eb
Vitor Ribeiro <vitorriblacerda@gmail.com>
2022-08-29 18:24:18 -0300

    Config and first program (Hello world)

---
+++ Basic Projects/HelloWorld/helloWorld/src/hello.java
@@ -0,0 +1,7 @@
+//Here is defined the "hello" class
+public class hello {
+    //Here is defined the main method
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //Printing the string
+    }
+}
