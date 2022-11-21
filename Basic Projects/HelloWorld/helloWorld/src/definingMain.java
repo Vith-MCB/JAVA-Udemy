a46662ed779bbc4b306c986819881c208340bfe4
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-21 16:37:48 -0300

    config

---
+++ Basic Projects/HelloWorld/helloWorld/src/definingMain.java
@@ -0,0 +1,13 @@
+public class definingMain {
+    //To run the main part of the code, the main should be declared
+    //"psvm" can be used to make it faster
+    //"public static void main(String[] args){}
+
+    //public: is an access modifier
+    //static: Java keyword that "defines" what method to run the code
+    //void: Indicates that the method will not return anything
+
+    public static void main(String[] args) {
+        System.out.println(1+2); //in that example, the code prints "3"
+    }
+}
