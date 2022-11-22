263fcddc4c70c5ee1dbfa1c5cbb908e456bf0a53
Vith-MCB <vitorriblacerda@gmail.com>
2022-11-21 23:50:16 -0300

    variables and data type

---
+++ Basic Projects/HelloWorld/helloWorld/src/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    //Variables are used to store information  in the computer
+    int myNum = 5;               // Integer (whole number)
+    float myFloatNum = 5.99f;    // Floating point number
+    char myLetter = 'D';         // Character
+    boolean myBool = true;       // Boolean
+    String myText = "Hello";     // String
+}
